499ab5dec6ae67c620a548a7fbdd5479e1db1f40
marcillocordovaramonoctavio-coder <marcillocordovaramonoctavio@gmail.com>
2026-02-18 22:59:03 -0500

    Add 3x3 matrix initialization and printing
    Initialize a 3x3 matrix and print its elements.

---
+++ matriz.py
@@ -0,0 +1,11 @@
+# Tarea: Matriz 3x3
+matriz = [
+   [2, 4, 6],
+   [1, 3, 5],
+   [7, 8, 9]
+]
+
+print("Imprimiendo la matriz:")
+for i in range(3):
+   for j in range(3):
+      print(f"Posición [{i}][{j}]: {matriz[i][j]}")
